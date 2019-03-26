c8ca236dc99dc8886343c3bcd8e1f61431495156
Matt Brigida <mdbrigida@gmail.com>
2019-03-26 08:34:13 -0400

    convert json to alist

--- individual_functions/get_company_info/iex_company_info.el
+++ individual_functions/get_company_info/iex_company_info.el
@@ -1,10 +1,14 @@
 
 
 (defun iex-company-info (ticker)
+    "Right now returns an alist. Can get an element of the alist via: (alist-get 'exchange the_alist)"
   (interactive)
   (setq the_data (with-current-buffer (url-retrieve-synchronously (concat "https://api.iextrading.com/1.0/stock/" ticker "/company"))
 		   (prog1
 		       (buffer-string)
 		     (kill-buffer))))
-  (car (last (split-string the_data "\n")))
+  (json-read-from-string (car (last (split-string the_data "\n"))))
   )
+
+
+(provide 'iex_company_info)\n;;; iex_company_info.el ends here
